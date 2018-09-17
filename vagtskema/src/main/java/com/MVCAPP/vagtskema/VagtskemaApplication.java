4d80c3c6424c5dfc6d5a4562f4fb49b4568ba998
Victor <Vict2904@stud.kea.dk>
2018-09-17 10:53:48 +0200

    Først Commit - Empty Project
    No changes - Spring Boot: Security, Web, MYSQL, Thyme

---
+++ vagtskema/src/main/java/com/MVCAPP/vagtskema/VagtskemaApplication.java
@@ -0,0 +1,12 @@
+package com.MVCAPP.vagtskema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagtskemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagtskemaApplication.class, args);
+	}
+}
