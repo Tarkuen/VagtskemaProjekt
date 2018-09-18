12939dc5a5f6aee4a6f1581d6b28e071a243899d
Victor <Vict2904@stud.kea.dk>
2018-09-18 15:50:27 +0200

    Fortsatte Tests

--- vagtskema/src/main/java/Model/Medarbejder.java
+++ vagtskema/src/main/java/Model/Medarbejder.java
@@ -5,6 +5,12 @@ import java.util.Date;
 public class Medarbejder {
 
    Tidsobjekt tidsObjekt;
+   Integer points = 0;
+
+    public Medarbejder(Tidsobjekt tidsObjekt, Integer points) {
+        this.tidsObjekt = tidsObjekt;
+        this.points = points;
+    }
 
     public Medarbejder(Tidsobjekt tidsObjekt) {
         this.tidsObjekt = tidsObjekt;
--- vagtskema/src/test/java/Model/MedarbejderTest.java
+++ vagtskema/src/test/java/Model/MedarbejderTest.java
@@ -3,7 +3,10 @@ package Model;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -20,22 +23,50 @@ public class MedarbejderTest {
 //        Assert.assertEquals(false, check);
 
         Vagt v = new Vagt(new Tidsobjekt(new Date(2018,10,18, 9,0 ), new Date(2018,10,18,15,0)));
-        Medarbejder m = new Medarbejder(new Tidsobjekt(new Date(2018,10,18, 15,0 ), new Date(2018,10,18,18,0)));
+        Medarbejder m = new Medarbejder(new Tidsobjekt(new Date(2018,10,18, 15,0 ), new Date(2018,10,18,18,0)), 1);
+        Medarbejder m1 = new Medarbejder(new Tidsobjekt(new Date(2018,10,18, 15,0 ), new Date(2018,10,18,18,0)));
 
-        compareDates(v, m);
+        ArrayList<Medarbejder> medarbejderlisten = new ArrayList();
+
+        medarbejderlisten.add(m);
+        medarbejderlisten.add(m1);
+
+        medarbejderlisten.sort(Comparator.comparing(o -> o.points));
+        for (Medarbejder medarbejder : medarbejderlisten) {
+            System.out.println(medarbejder.points);
+        }
+
+        compareDates(v, medarbejderlisten);
+
+
+
 
     }
 
 
-    public void compareDates(Vagt v, Medarbejder m){
+    public void compareDates(Vagt v, ArrayList<Medarbejder> list){
         int check = 0;
 //        Forslået Vagt
-        Date a1 = v.tidsrumObjekt.startTid;
-        Date a2 = v.tidsrumObjekt.slutTid;
+        for(Medarbejder m : list){
+            Date a1 = v.tidsrumObjekt.startTid;
+            Date a2 = v.tidsrumObjekt.slutTid;
 
 //        Medarbejder vagt
-        Date b1 = m.tidsObjekt.startTid;
-        Date b2 = m.tidsObjekt.slutTid;
+            Date b1 = m.tidsObjekt.startTid;
+            Date b2 = m.tidsObjekt.slutTid;
+
+            if(a1.after(b2) || check==a1.compareTo(b2)){
+                System.out.println("Eligible");
+            }
+            else if(a2.before(b1) || check==a2.compareTo(b1)){
+                System.out.println("Eligible");
+            }
+
+            else{
+                System.out.println("Not eligible");
+            }
+        }
+
 
 //        A1 > B2
 //        a1.after(b2);
@@ -50,17 +81,9 @@ public class MedarbejderTest {
 //         A2 = B1
 //         if(check == a2.compareTo(b1)){ }
 
-         if(a1.after(b2) || check==a1.compareTo(b2)){
-             System.out.println("Eligible");
-         }
-         else if(a2.before(b1) || check==a2.compareTo(b1)){
-             System.out.println("Eligible");
-         }
-
-         else{
-             System.out.println("Not eligible");
-         }
 
 
     }
+
+
 }
